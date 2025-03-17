b55755d7fad876b255c7dc9f168b33eea7ce0986
Graham Preston <graham.preston@gmail.com>
2025-03-17 10:29:38 +0100

    Fix Randall

--- src/components/viewer/Stage.tsx
+++ src/components/viewer/Stage.tsx
@@ -234,7 +234,7 @@ function YoshisStory() {
       1022: [-14.954894065856934, -103.4649963378906],
     };
     // return frameNumber to -123 based.
-    const frameInLap = (spectateStore.frame - 123 + 1200) % 1200;
+    const frameInLap = ((spectateStore.frame ?? 0) - 123 + 1200) % 1200;
     const randallWidth = 11.9;
 
     if (frameInLap > 476 && frameInLap < 1016) {
